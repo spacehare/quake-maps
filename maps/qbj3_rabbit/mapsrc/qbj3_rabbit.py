cb592dcec7514461803cb8daf39bc2158d490a39
spacehare <dantath425@gmail.com>
2025-11-12 15:40:03 -0500

    move logic from script package into local per-map files

--- maps/qbj3_rabbit/mapsrc/qbj3_rabbit.py
+++ maps/qbj3_rabbit/mapsrc/qbj3_rabbit.py
@@ -1,29 +1,70 @@
-def main(context: dict):
-    print("🐇 running qbj3_rabbit.py")
+# this Python file contains code to pre-process the MAP file
 
-    var_prefix = context["var_prefix"]
-    entities = context["entities"]
+# https://github.com/spacehare/rabbit_quake
 
-    for ent in entities:
+
+import copy
+
+
+def clip(ent) -> list:
+    output_brushes: list = []
+    for brush in ent.brushes:
+        clone = copy.deepcopy(brush)
+        for plane in clone.planes:
+            plane.texture_name = 'clip'
+        output_brushes.append(clone)
+    return output_brushes
+
+
+def main(context: dict) -> list:
+    print('🐇 running qbj3_rabbit.py')
+
+    var_prefix = context['var_prefix']
+    input_entities = context['entities']
+    output_entities: list = []
+
+    print('prefix: %s' % var_prefix)
+
+    assert input_entities[0].classname == 'worldspawn'
+
+    worldspawn = input_entities[0]
+
+    for ent in input_entities:
+        if ent.kv.get(var_prefix + 'delete') == '1':
+            continue
+        else:
+            output_entities.append(ent)
+
+        for key in ent.kv:
+            if ent.kv[key].startswith('eval'):
+                ent.kv[key] = eval(ent.kv[key].removeprefix('eval'))
+
+        classname: str = ent.kv['classname']
+
+        if ent.kv.get(var_prefix + 'clip') == '1':
+            worldspawn.brushes += clip(ent)
+
         # door
-        if ent.kv["classname"] == "func_door":
-            ent.kv.setdefault("_minlight", "50")
-            ent.kv.setdefault("sounds", "3")
-            ent.kv.setdefault("_dirt", "-1")
+        if classname == 'func_door':
+            ent.kv.setdefault('_minlight', '50')
+            ent.kv.setdefault('sounds', '3')
+            ent.kv.setdefault('_dirt', '-1')
 
         # purge angles
         if (
-            ent.kv["classname"].startswith("trigger")
-            and not ent.kv.get(var_prefix + "use_angle") == "1"
-            and ent.kv.get("angle")
-            and not ent.kv["classname"].endswith("monsterjump")
+            classname.startswith('trigger')
+            and not ent.kv.get(var_prefix + 'use_angle') == '1'
+            and ent.kv.get('angle')
+            and not classname.endswith('monsterjump')
         ):
-            print("clearing angle on %s" % ent.kv["classname"])
-            del ent.kv["angle"]
+            print('clearing angle on %s' % classname)
+            del ent.kv['angle']
 
         # texture swapping
-        if tex := ent.kv.get(var_prefix + "replace_texture_from"):
+        if tex := ent.kv.get(var_prefix + 'replace_texture_from'):
             for brush in ent.brushes:
                 for face in brush.planes:
                     if face.texture_name == tex:
-                        face.texture_name = ent.kv[var_prefix + "replace_texture_with"]
+                        face.texture_name = ent.kv[var_prefix + 'replace_texture_with']
+
+    return output_entities
