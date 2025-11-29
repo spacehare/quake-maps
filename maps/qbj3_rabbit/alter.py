0f2eff43c56682f3282d9568e5afde25b20fbd62
spacehare <dantath425@gmail.com>
2025-11-29 18:08:36 -0500

    use script to move autosave triggers into new layer

--- maps/qbj3_rabbit/alter.py
+++ maps/qbj3_rabbit/alter.py
@@ -5,7 +5,7 @@ from rabbitquake.app.parse import dumps, loads
 AUTOSAVE_LAYER_ID = '1124538'
 
 if __name__ == '__main__':
-    level_path: Path = Path(Path(__file__) / 'mapsrc/qbj3_rabbit.map')
+    level_path: Path = Path(Path(__file__).parent / 'mapsrc/qbj3_rabbit.map')
     output_path: Path = level_path.with_stem(level_path.stem + '_altered')
 
     entities = loads(level_path.read_text())
@@ -13,9 +13,7 @@ if __name__ == '__main__':
     for ent in entities:
         for key, value in ent.kv.items():
             if key.startswith('target') and value == '_AUTOSAVE':
+                ent.kv['_tb_layer'] = AUTOSAVE_LAYER_ID
                 break
-        else:
-            ent.kv['_tb_layer'] = AUTOSAVE_LAYER_ID
-        continue
 
     output_path.write_text(dumps(entities))
