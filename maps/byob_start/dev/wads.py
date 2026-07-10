c2b74a11223de2b6d7d6e2d6311457d90b443b46
spacehare <dantath425@gmail.com>
2026-07-10 17:31:14 -0400

    hardcode new list of wads to rename

--- maps/byob_start/dev/wads.py
+++ maps/byob_start/dev/wads.py
@@ -1,16 +1,5 @@
-import csv
 import subprocess
 from pathlib import Path
-
-SUBMISSIONS = Path(Path(__file__).parent / r'..\mapsrc\BYOB - submissions.tsv')
-
-
-# https://docs.python.org/3/library/csv.html
-def read_csv() -> list[dict]:
-    reader = csv.DictReader(SUBMISSIONS.read_text().splitlines(), delimiter='\t')
-    obj = [row for row in reader]
-    return obj
-
 
 p_byob = Path(r'I:\byob')
 p_wads = Path(p_byob / 'wads')
@@ -28,13 +17,41 @@ tools = [
     'lavaskip',
 ]
 
-
-tools2 = []
-for tex in tools:
-    fbr = tex + '_fbr'
-    tools2.append(tex)
-tools += tools2
-del tools2
+stuff = {
+    'auh_dev': 'auh_dev',
+    'auh_liquids': 'auh_liq',
+    'auh_nature': 'auh_nat',
+    'auh_skies': 'auh_ski',
+    'auh_tools': 'auh_too',
+    'auh_walls': 'auh_wal',
+    'byobchumastic': 'chum',
+    'byob_4lt': '4lt',
+    'byob_avix': 'avix',
+    'byob_cc': 'cc',
+    'byob_chaosed0': 'chao',
+    'byob_chaosed0_tool': 'chao_too',
+    'byob_dicedpinecones': 'diced',
+    'byob_EHOT': 'ehot',
+    'byob_gnorlio': 'gnor',
+    'byob_haikids': 'haik',
+    'byob_naitelveni': 'nait',
+    'byob_novafrost': 'nova',
+    'byob_pm': 'pm',
+    'byob_puppluka_lauravix': 'pupp',
+    'byob_rabbit': 'rabb',
+    'byob_rabbit_dev': 'rabb_dev',
+    'byob_rabbit_proto': 'rabb_pro',
+    'byob_redeadita': 'rede',
+    'byob_roj': 'roj',
+    'byob_squidv': 'squi',
+    'byob_wons': 'wons',
+    'chornobyl': 'rory',
+    'crayons1': 'quie',
+    'library': 'clen',
+    'moko': 'moko',
+    'razzzor_tex': 'razz',
+    'shinko_zed666': 'shin',
+}
 
 
 def extract():
@@ -58,31 +75,28 @@ def find_dupes():
 
 
 def do_it():
-    for person in read_csv():
-        pattern = person['WAD filename pattern'][:-4]
-        if not pattern:
-            continue
+    for k, v in stuff.items():
+        folder = p_extracted / k
+        i = 0
+        for file in folder.iterdir():
+            new_name = f'{v}_{i:03}'
 
-        for folder in p_extracted.iterdir():
-            if not folder.match(pattern):
-                continue
-            i = 0
-            for file in folder.iterdir():
-                new_name = f'{person["nickname"][:4].lower()}_{i:03}'
+            if file.stem.endswith('_fbr'):
+                new_name += '_fbr'
 
-                if file.stem.endswith('_fbr'):
-                    new_name += '_fbr'
+            if file.stem.startswith('star_'):
+                new_name = 'star_' + new_name
+            elif file.stem.startswith('plus_'):
+                new_name = 'plus_' + file.stem[5] + new_name
+            elif file.stem.startswith('_') and file.stem.endswith('_fbr'):
+                new_name = '{' + new_name
 
-                if file.stem.startswith('star_'):
-                    new_name = 'star_' + new_name
-                elif file.stem.startswith('plus_'):
-                    new_name = 'plus_' + file.stem[5] + new_name
-                elif file.stem.startswith('_') and file.stem.endswith('_fbr'):
-                    new_name = '{' + new_name
-
-                file.copy(p_renamed / (new_name + file.suffix))
-                i += 1
+            file.copy(p_renamed / (new_name + file.suffix))
+            i += 1
 
 
 def pack():
     subprocess.run(['qpakman', str(p_renamed) + r'\*', '-o', p_merged_wad])
+
+
+do_it()
